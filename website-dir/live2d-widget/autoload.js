90f10417ee31da630309e2822f24f983508a57e3
idoz <96138710+QkongZ@users.noreply.github.com>
2023-07-23 23:55:42 +0800

    Update autoload.js

--- website-dir/live2d-widget/autoload.js
+++ website-dir/live2d-widget/autoload.js
@@ -1,6 +1,6 @@
 // live2d_path 参数建议使用绝对路径
 const live2d_path = "https://fastly.jsdelivr.net/gh/stevenjoezhang/live2d-widget@latest/";
-const live2d_path2 = "https://fastly.jsdelivr.net/gh/QkongZ/myCFworker/website-dir/live2d-widget/";
+const live2d_path2 = "https://fastly.jsdelivr.net/gh/QkongZ/CFworker_pages/website-dir/live2d-widget/";
 //const live2d_path = "/live2d-widget/";
 
 // 封装异步加载资源的方法
@@ -35,8 +35,8 @@ if (screen.width >= 100) {
 		// 配置选项的具体用法见 README.md
 		initWidget({
 			waifuPath: live2d_path + "waifu-tips.json",
-			apiPath: "https://live2d.fghrsh.net/api/",
-			//cdnPath: "https://fastly.jsdelivr.net/gh/fghrsh/live2d_api/",
+			//apiPath: "https://live2d.fghrsh.net/api/",
+			cdnPath: "https://fastly.jsdelivr.net/gh/fghrsh/live2d_api/",
 			tools: ["hitokoto", "asteroids", "switch-model", "switch-texture", "photo", "info", "quit"]
 		});
 	});
